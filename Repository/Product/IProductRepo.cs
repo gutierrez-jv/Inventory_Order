2f769633a19eb0f86749cb1b3598b63b765b0600
jv-gutierrez <gutierrez.jan.vince@gmail.com>
2026-03-19 19:31:31 +0800

    Made controllers and made the product repo

--- Repository/Product/IProductRepo.cs
+++ Repository/Product/IProductRepo.cs
@@ -1,6 +1,14 @@
-﻿namespace Inventory_Order.Repository.ProductRepository
+﻿using Inventory_Order.Models.Database;
+
+namespace Inventory_Order.Repository.ProductRepository
 {
     public interface IProductRepo
     {
+        List<ProductTb> GetAllProducts();
+        ProductTb? GetProductById(int id);
+        ProductTb? GetProductByBarcode(string barcode);
+        void AddProduct(ProductTb product);
+        void UpdateProduct(ProductTb product);
+        void DeleteProduct(int id);
     }
 }
--- Repository/Product/ProductRepo.cs
+++ Repository/Product/ProductRepo.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Inventory_Order.Models.Database;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
 
 namespace Inventory_Order.Repository.ProductRepository
 {
-    public class ProductRepo : Controller
+    public class ProductRepo : IProductRepo
     {
-        public IActionResult Index()
+        private readonly InventoryOrderDbContext _context;
+
+        public ProductRepo(InventoryOrderDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public void AddProduct(ProductTb product)
+        {
+            _context.ProductTbs.Add(product);
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var ex = _context.ProductTbs.Find(id);
+            if (ex != null) _context.ProductTbs.Remove(ex);
+            _context.SaveChanges();
+        }
+
+        public List<ProductTb> GetAllProducts()
+        {
+            return _context.ProductTbs.ToList();
+        }
+
+        public ProductTb? GetProductByBarcode(string barcode)
+        {
+            _context.ProductTbs.Where(p => p.Barcode == barcode).FirstOrDefault();
+            return null;
+        }
+
+        public ProductTb? GetProductById(int id)
+        {
+            _context.ProductTbs.Where(p => p.ProductsId == id).FirstOrDefault();
+            return null;
+        }
+        public void UpdateProduct(ProductTb product)
+        {
+            _context.ProductTbs.Update(product);
         }
     }
 }
